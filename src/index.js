7ae76b566049adbd74e5bbfd6a33f94b8bbb3b4f
Soleny Yumare <solenyyumare@gmail.com>
2022-01-13 17:33:37 +0000

    vowel changer

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function changeVowels() {
+  const value = document.querySelector(".texto-value").value;
+  let result = value.replace(/[aeou]/gi, "i");
+  const usedDiv = document.querySelector(".result");
+  const createP = document.createElement("p");
+  usedDiv.insertAdjacentElement("beforeend", createP);
+  createP.textContent = result;
+}
+
+const sendForm = document.querySelector(".btn");
+sendForm.addEventListener("click", changeVowels);
+
+document.querySelector("form").onsubmit = (e) => {
+  e.target.reset();
+  return false;
+};
